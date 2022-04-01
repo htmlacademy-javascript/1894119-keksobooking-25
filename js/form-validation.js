dd29d5dbcd18967d5a2b512fda21b7ecf02d153a
Darya <dasha8562@gmail.com>
2022-04-01 18:33:23 +0300

    Исправление ошибок

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,18 +1,25 @@
-const MIN_PRICE = {
-  'bungalow': 0,
-  'flat': 1000,
-  'hotel': 3000,
-  'house': 5000,
-  'palace': 10000,
-};
+import {locationLat, locationLng} from './data.js';
+
 const MAX_PRICE = 100000;
 const MAX_ROOMS = 100;
+const MIN_GUESTS = 0;
+
+const minPrice = {
+  bungalow: '0',
+  flat: '1000',
+  hotel: '3000',
+  house: '5000',
+  palace: '10000',
+};
 
 const adForm = document.querySelector('.ad-form');
 const price = adForm.querySelector('[name="price"]');
 const type = adForm.querySelector('[name="type"]');
 const rooms = adForm.querySelector('[name="rooms"]');
 const capacity = adForm.querySelector('[name="capacity"]');
+const address = adForm.querySelector('[name="address"]');
+
+address.value = `${locationLat}, ${locationLng}`;
 
 const capacityOptions = {
   '1' : '1',
@@ -30,22 +37,25 @@ const pristine = new Pristine(adForm, {
   errorTextClass: 'ad-form__error-text',
 });
 
-const validatePrice = (value) => value >= MIN_PRICE[type.value] && value <= MAX_PRICE;
+const validatePrice = (value) => value >= Number(minPrice[type.value]) && value <= MAX_PRICE;
 
 const getValidatePriceErrorMessage = () => {
   if (price.value >= MAX_PRICE) {
     return `Максимальная цена за ночь — ${MAX_PRICE}`;
   }
-  if (price.value <= MIN_PRICE[type.value]) {
-    return `Минимальная цена за ночь для выбранного типа жилья — ${MIN_PRICE[type.value]}`;
+  if (price.value <= Number(minPrice[type.value])) {
+    return `Минимальная цена за ночь для выбранного типа жилья — ${Number(minPrice[type.value])}`;
   }
 };
 
 const validateCapacity = () => capacityOptions[rooms.value].includes(capacity.value);
 
 const getValidateCapacityErrorMessage = () => {
-  if (rooms.value === MAX_ROOMS) {
+  if (Number(rooms.value) === MAX_ROOMS) {
     return 'Не для гостей';
+  }
+  if (Number(capacity.value) === MIN_GUESTS) {
+    return 'Доступно только 100 комнат';
   }
   return 'Количество гостей не должно превышать количество комнат';
 };
@@ -55,7 +65,7 @@ pristine.addValidator(price, validatePrice, getValidatePriceErrorMessage);
 pristine.addValidator(capacity, validateCapacity, getValidateCapacityErrorMessage);
 
 type.addEventListener('change', () => {
-  price.placeholder =  MIN_PRICE[type.value];
+  price.placeholder =  minPrice[type.value];
   pristine.validate(price);
 });
 
